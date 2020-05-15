3c38630f861f5b8d43b454ef170b29b44c94523e
Micha de Vries <contact@michadevries.nl>
2020-05-15 15:19:59 +0000

    Release v1.0.0: Initial commit

---
+++ index.js
@@ -0,0 +1,132 @@
+const Rable = require('rable.js');
+const RableAPI = require(__dirname + '/lib');
+const app = new Rable({
+	port: 3002
+});
+
+//bind the API controller to the Rable instance.
+const api = new RableAPI(app);
+api.defaultMethod('get');
+
+//create the API(s)
+var exampleAPI = api.createAPI('example');
+var dateAPI = api.createAPI('date');
+
+//register the Actions for API 'example'.
+	exampleAPI.registerAction('plain', (req, res) => res.json({
+		success: true,
+		message: 'You have reached the first API!'
+	}));
+
+	exampleAPI.registerAction('params', (req, res) => res.json({
+		success: true,
+		message: 'Here are the provided params',
+		result: req.params
+	}), {
+		params: ':first/second'
+	});
+
+	exampleAPI.registerAction('query', (req, res) => res.json({
+		success: true,
+		result: req.query
+	}));
+
+	exampleAPI.registerAction('body', (req, res) => res.json({
+		success: true,
+		result: req.body
+	}), 'post');
+
+	exampleAPI.registerAction('actions', (req, res) => {
+		var acs = exampleAPI.listActions();
+		var final = [];
+
+		acs.forEach(ac => {
+			ac = exampleAPI.obtainAction(ac.name, ac.method);
+			final.push({
+				name: ac.name,
+				method: ac.method,
+				params: ac.params
+			});
+		})
+
+		res.json({
+			success: true,
+			result: final
+		})
+	})
+
+//register the Actions for API 'date'.
+	dateAPI.registerAction('year', (req, res) => res.json({
+		success: true,
+		result: new Date().getFullYear()
+	}));
+
+	dateAPI.registerAction('day', (req, res) => res.json({
+		success: true,
+		result: new Date().getDate()
+	}));
+
+	dateAPI.registerAction('month', (req, res) => res.json({
+		success: true,
+		result: new Date().getMonth() + 1 //months start at 0 in javascript.
+	}));
+
+	dateAPI.registerAction('date', (req, res) => res.json({
+		success: true,
+		result: [new Date().getMonth() + 1, new Date().getDate(), new Date().getFullYear()].join('-')
+	}));
+
+	dateAPI.registerAction('hour', (req, res) => res.json({
+		success: true,
+		result: new Date().getHours()
+	}));
+
+	dateAPI.registerAction('minute', (req, res) => res.json({
+		success: true,
+		result: new Date().getMinutes()
+	}));
+
+	dateAPI.registerAction('second', (req, res) => res.json({
+		success: true,
+		result: new Date().getSeconds()
+	}));
+
+	dateAPI.registerAction('time', (req, res) => res.json({
+		success: true,
+		result: [new Date().getHours(), new Date().getMinutes(), new Date().getSeconds()].join(':')
+	}));
+
+	dateAPI.registerAction('full', (req, res) => res.json({
+		success: true,
+		result: [new Date().getMonth() + 1, new Date().getDate(), new Date().getFullYear()].join('-') + ' ' + [new Date().getHours(), new Date().getMinutes(), new Date().getSeconds()].join(':')
+	}));
+
+	dateAPI.registerAction('actions', (req, res) => {
+		var acs = dateAPI.listActions();
+		var final = [];
+
+		acs.forEach(ac => {
+			ac = dateAPI.obtainAction(ac.name, ac.method);
+			final.push({
+				name: ac.name,
+				method: ac.method,
+				params: ac.params
+			});
+		})
+
+		res.json({
+			success: true,
+			actions: final
+		})
+	})
+
+
+
+app.info.set('name', 'rable.js-api ' + api.version + ' showcase');
+app.info.set('version', api.version);
+
+app.template.setElement('navbar', __dirname + '/elements/navbar.rbl');
+app.template.setElement('footer', __dirname + '/elements/footer.rbl');
+
+app.get('/', __dirname + '/views/home.html');
+app.static('/docs', __dirname + '/docs/rable.js-api', {noTemplate: true});
---
+++ lib/README.md
@@ -0,0 +1,28 @@
+# Rable
+Rable.js-api is the official API extention to rable.js
+
+## Installation
+Use the package manager [npm](https://npmjs.com) to install rable.js and rable.js-api.
+```bash
+npm install rable.js rable.js-api
+```
+
+## Example files
+The index.js and the files within /views/ and /elements/ are there as an example and being used in development. In case you want to to try it out, do as follows:
+For this example git and npm need to be installed on your system.
+```bash
+    git clone https://github.com/kearfy/rable.js-api rablejs-api-example && cd rablejs-api-example && npm install
+    node .
+```
+The example is now available through port 3002.
+You can also take a look at the publicly hosted example over here: https://rable-api.app.michadevries.nl
+
+## Documentation
+See https://rable-api.app.michadevries.nl/docs/ for further documentation.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+Please make sure to update tests as appropriate.
+
+## License
+[MPL-2.0](https://www.mozilla.org/en-US/MPL/2.0/)
